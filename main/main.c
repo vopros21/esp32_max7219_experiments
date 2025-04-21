1efc9fb9523168d1862b9dacb599a90b3ec71e4f
Mike Kostenko <vopros.m@gmail.com>
2025-04-21 22:51:24 +0100

    Refactor run_text function for clarity and remove unused code

--- main/main.c
+++ main/main.c
@@ -42,7 +42,44 @@ static const uint64_t symbols[] = {
     0x3c66607c66663c00,
     0x3c66666e76663c00
 };
-static const size_t symbols_size = sizeof(symbols) - sizeof(uint64_t) * CASCADE_SIZE;
+// static const size_t symbols_size = sizeof(symbols) - sizeof(uint64_t) * CASCADE_SIZE;
+
+void run_text(max7219_t *dev, const uint64_t val)
+{
+    uint8_t s[8];
+    s[7] = val & 0xFF;
+    s[6] = (val >> 8) & 0xFF;
+    s[5] = (val >> 16) & 0xFF;
+    s[4] = (val >> 24) & 0xFF;
+    s[3] = (val >> 32) & 0xFF;
+    s[2] = (val >> 40) & 0xFF;
+    s[1] = (val >> 48) & 0xFF;
+    s[0] = (val >> 56) & 0xFF;
+
+    
+    for (int i = 0; i < 8; i++) {
+            max7219_set_digit(dev, i, 0);
+    }
+
+    int row = 31;
+    while (row > 0) {
+        for (int j = 7; j >= 0; j--) {
+            for (int i = 0; i < 8; i++) {
+                if (row <= 23) {
+                    max7219_set_digit(dev, row + 8 - i, s[i] >> (8 - j));
+                }
+                max7219_set_digit(dev, row - i, s[i] << j);
+            }
+
+            vTaskDelay(pdMS_TO_TICKS(200));
+        }
+        for (int i = 0; i < 8; i++) {
+            if (row <= 23)
+                max7219_set_digit(dev, row + 8 - i, 0);
+        }
+        row -= 8;
+    }
+}
 
 void task(void *pvParameter)
 {
@@ -67,21 +104,45 @@ void task(void *pvParameter)
     ESP_ERROR_CHECK(max7219_init_desc(&dev, HOST, MAX7219_MAX_CLOCK_SPEED_HZ, PIN_CS));
     ESP_ERROR_CHECK(max7219_init(&dev));
 
-    size_t offs = 0;
+    // size_t offs = 0;
+    // uint8_t row = 27;
+    // uint8_t value = 0xC0;
     while (1)
     {
-        printf("---------- draw\n");
+        printf("---------- draw: %d \n", dev.digits);
         max7219_set_brightness(&dev, 0);
+        
+        run_text(&dev, symbols[1]);
 
-        for (uint8_t c = 0; c < CASCADE_SIZE; c++) {
-            max7219_draw_image_8x8(&dev, c * 8, (uint8_t *)symbols + c * 8 + offs);
-            // max7219_set_brightness(&dev, c % 16);
-            vTaskDelay(pdMS_TO_TICKS(200));
-        }
-        offs += 8;
-        if (offs >= symbols_size + 8)
-            offs = 0;
-        vTaskDelay(pdMS_TO_TICKS(CONFIG_EXAMPLE_SCROLL_DELAY));
+        // for (uint8_t c = 0; c < CASCADE_SIZE; c++) {
+        //     max7219_draw_image_8x8(&dev, c * 8, (uint8_t *)symbols + c * 8 + offs);
+        //     // max7219_set_brightness(&dev, c % 16);
+        //     vTaskDelay(pdMS_TO_TICKS(200));
+        // }
+        // offs += 8;
+        // if (offs >= symbols_size + 8)
+        //     offs = 0;
+
+
+
+        // max7219_set_digit(&dev, row, value);
+        // vTaskDelay(pdMS_TO_TICKS(200));
+        // value >>= 1;
+        // if (value == 0x01) {
+        //     uint8_t new_row = row - 8 > dev.digits ? 27 : row - 8;
+        //     max7219_set_digit(&dev, new_row, 0x80);
+        // }
+        // if (value == 0) {
+        //     value = 0xC0;
+        //     max7219_set_digit(&dev, row, 0);
+        //     row -= 8;
+        //     if (row > dev.digits) {
+        //         row = 27;
+        //     }
+        // }
+
+
+
 
         // if (offs == symbols_size)
         //     offs = 0;
