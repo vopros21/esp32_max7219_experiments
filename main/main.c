36c05205ca5a69ef8443fc987f59f061949e364c
Mike Kostenko <vopros.m@gmail.com>
2025-05-31 11:32:53 +0100

    Add alphabet header and update main.c to use symbol definitions

--- main/main.c
+++ main/main.c
@@ -4,6 +4,7 @@
 #include <esp_idf_version.h>
 #include <max7219.h>
 #include <esp_idf_lib_helpers.h>
+#include "alphabet.h"
 
 #define HOST HELPER_SPI_HOST_DEFAULT
 
@@ -16,32 +17,6 @@
 #define PIN_CS 5
 #define PIN_CLK 6
 
-static const uint64_t symbols[] = {
-    // letters
-    0x383838fe7c381000,
-    0xc3c3c3c3dbe7c3c3, // M
-    0x3c18181818001800, // i
-    0x66361E0E1E360600, // k
-    0x3c023e221c000000, // e
-
-    0x383838fe7c381000, // arrows
-    0x10387cfe38383800,
-    0x10307efe7e301000,
-    0x1018fcfefc181000,
-    0x10387cfefeee4400, // heart
-    0x105438ee38541000, // sun
-
-    0x7e1818181c181800, // digits
-    0x7e060c3060663c00,
-    0x3c66603860663c00,
-    0x30307e3234383000,
-    0x3c6660603e067e00,
-    0x3c66663e06663c00,
-    0x1818183030667e00,
-    0x3c66663c66663c00,
-    0x3c66607c66663c00,
-    0x3c66666e76663c00
-};
 // static const size_t symbols_size = sizeof(symbols) - sizeof(uint64_t) * CASCADE_SIZE;
 
 
@@ -119,7 +94,8 @@ void task(void *pvParameter)
         printf("---------- draw: %d \n", dev.digits);
         max7219_set_brightness(&dev, 0);
         
-        run_text(&dev, symbols[1]);
+        run_text(&dev, symbols[2]);
+        run_text(&dev, symbols[22]);
 
         // for (uint8_t c = 0; c < CASCADE_SIZE; c++) {
         //     max7219_draw_image_8x8(&dev, c * 8, (uint8_t *)symbols + c * 8 + offs);
